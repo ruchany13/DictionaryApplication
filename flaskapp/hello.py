f79f8484893a0a452f80b9671a641d25e8ffe479
Ruchan Yalçın <ruchany13@gmail.com>
2023-02-18 22:19:03 +0300

    add new template for app

--- flaskapp/hello.py
+++ flaskapp/hello.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template('base_index.html')
